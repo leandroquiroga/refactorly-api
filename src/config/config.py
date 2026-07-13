8f61905eb57e3eee692262d0eb23253155ec970d
leandroquiroga <leandroquiroga9514@gmail.com>
2026-07-13 19:27:24 -0300

    fix GEMINI_API_KEY variable name and add DEEPSEEK_API_KEY to configuration and implement BaseLLMProvider class with common logic for LangChain based LLM providers

--- src/config/config.py
+++ src/config/config.py
@@ -6,7 +6,8 @@ class Settings(BaseSettings):
     """Application settings loaded and validated from environment/.env file."""
 
     OPENAI_API_KEY: str = Field(..., min_length=1, validation_alias=("OPENAI_API_KEY"), description="OpenAI API Key")
-    GEMINI_APY_KEY: str = Field(...,  min_length=1, validation_alias=("GOOGLE_API_KEY"), description="Gemini API Key")
+    GEMINI_API_KEY: str = Field(...,  min_length=1, validation_alias=("GOOGLE_API_KEY"), description="Gemini API Key")
+    DEEPSEEK_API_KEY: str = Field(..., min_length=1, validation_alias=("DEEPSEEK_API_KEY"), description="Deepseek API Key")
     DEFAULT_PROVIDER: str = Field(..., min_length=1, validation_alias=("DEFAULT_PROVIDER"), description=("Default LMM Provider (openai | gemini | deepseek)"))
     DEFAULT_MODEL: str = Field(..., min_length=1, validation_alias=("DEFAULT_MODEL"), description=("Default LMM model name"))
     LLM_TEMPERATURE: str = Field(..., ge=0.0, le=2.0, validation_alias=("LLM_TEMPERATURE"), description=("LLM temperature (0.0 to 2.0)"))
