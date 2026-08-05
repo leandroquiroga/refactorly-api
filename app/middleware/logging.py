7e8eff329b180edad4c7904306fb6fed51bda4d3
leandroquiroga <leandroquiroga9514@gmail.com>
2026-08-05 17:17:54 -0300

    the name of the dispatch method in RequestIdMiddleware has been corrected.

--- app/middleware/logging.py
+++ app/middleware/logging.py
@@ -13,7 +13,7 @@ logger = structlog.get_logger()
 class RequestIdMiddleware(BaseHTTPMiddleware):
     """Attach a unique request_id to every request for tracing"""
     
-    async def dispath(self, request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
+    async def dispatch(self, request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
         request_id = str(uuid.uuid4())
         
         structlog.contextvars.bind_contextvars(request_id=request_id)
